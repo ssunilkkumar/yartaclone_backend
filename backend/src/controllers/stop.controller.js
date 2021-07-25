fbd126b84651beee9ec3dfd6671bc31ac538e695
Sunil Kumar <ssunil.uk@gmail.com>
2021-07-25 10:40:11 +0530

    added

---
+++ backend/src/controllers/stop.controller.js
@@ -0,0 +1,60 @@
+const express = require ("express");
+const router = express.Router();
+
+const Stop = require ("../models/stops.model")
+
+
+router.get("/", async (req, res) => {
+    const stops = await Stop.find().lean().exec();
+
+    return res.status(200).json({data: stops})
+})
+
+router.get("/id/:id", async (req, res) => {
+    try{
+        const stop = await Stop.findById(req.params.id).lean().exec();
+        res.status(400).json({stop})
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
+})
+
+router.get("/cities", async (req, res) => {
+    try{
+        const all = []
+        const stop = await Stop.find().select("city").lean().exec();
+        stop.forEach(el => {
+            all.push(el.city)
+        })
+        res.status(400).json({all})
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
+})
+
+router.post("/", async (req, res) => {
+    const stop = await Stop.create(req.body);
+
+    return res.status(201).json({data: stop})
+})
+
+router.patch("/:id/update", async (req, res) => {
+    try{
+        const stop = await Stop.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(400).json({stop})
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const stop = await Stop.findByIdAndDelete(req.params.id)
+        res.status(400).json({stop})
+    } catch (err) {
+        res.status(400).json({message: err.message})
+
+    }
+})
+
+module.exports = router
